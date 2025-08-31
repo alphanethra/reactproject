b2914bcc4f54fc64b8495d8e626a49a7a9f118ae
143855 <treddy582@gmail.com>
2025-08-31 20:54:44 +0530

    job

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './Components/Home';
-import Login from './Components/auth/Login';
-import Signup from './Components/auth/Signup';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Navbar from './Components/shared/Navbar'
+import Home from './Components/Home'
+import Login from './Components/auth/Login'
+import Signup from './Components/auth/Signup'
+import Job from './Components/ui/Job'
 
 const appRouter = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
+    element:<Home />
   },
   {
     path: '/login',   // URL will be /login
-    element: <Login />,
+    element:<Login />
   },
   {
     path: '/signup',  // URL will be /signup
-    element: <Signup />,
+    element:<Signup />
   },
-]);
+  {
+    path:"/jobs",  //URL wil be /jobs
+    element:<Job />
+   },
+ 
+])
 
 function App() {
-  return <RouterProvider router={appRouter} />;
+  return
+  (
+    <div>
+    <RouterProvider router= {appRouter}/>
+    </div>
+  )
 }
 
 export default App;
